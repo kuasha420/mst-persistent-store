d59889e98ce8dbcd1c7741cdd108f63ad0bc2d04
Arafat Zahan <arafat.zahan@10up.com>
2023-07-12 04:17:41 +0600

    Update deps.

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -12,7 +12,7 @@ export function debounce<F extends Procedure>(
   waitMilliseconds = 50,
   options: Options = {
     isImmediate: false,
-  },
+  }
 ): (this: ThisParameterType<F>, ...args: Parameters<F>) => void {
   let timeoutId: ReturnType<typeof setTimeout> | undefined;
 
